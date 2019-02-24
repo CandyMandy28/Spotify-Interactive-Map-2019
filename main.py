745dd02d4bd4e901dbf00cdd5bf36561e6b0e08e
Charles Kuch <charlie.kuch@gmail.com>
2019-02-23 22:05:54 -0600

    helper methods for spotify stuffs

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import spotipy
 import config
+import spotify_ops
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_credentials_manager = SpotifyClientCredentials(client_id=config.client_id, client_secret=config.client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+sp = spotify_ops.sp
 
-# playlists = sp.user_playlists('spotify')
-# while playlists:
-#     for i, playlist in enumerate(playlists['items']):
-#         print( (i + 1 + playlists['offset'], playlist['uri'],  playlist['name'])) #"%4d %s %s" %
-#     if playlists['next']:
-#         playlists = sp.next(playlists)
-#     else:
-#         playlists = None
-
-
+playlists = sp.user_playlists('spotify')
+print(playlists)
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    if playlists['next']:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
