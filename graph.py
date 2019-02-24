b1bc67fd224490efbd30acdaa504bd1600c4251a
CandyMandy28 <amanda.wang294@gmail.com>
2019-02-24 02:09:30 -0600

    commit again for test

--- graph.py
+++ graph.py
@@ -3,20 +3,33 @@ import network
 import artist
 
 graph = Graph(directed = False)
+a.view()
 
-def startGraph(artist):
-    graph.view()
-    firstArtist(artist)
+# adding nodes
+a.addNode()
+a.addNode(10)
+a.addNode(11,color='red')
+a.addNode('john',shape='star',color='green')
 
-def firstArtist(artist):
-    graph.addNode(artist.name, radius=20)
+# adding edges
+a.addEdge(1,10)
+a.addEdge(1,'john',style='dash-line')
+a.addEdge(1,11,width='4')
+a.addEdge(10,11,width='2',label='hello world')
 
-def addArtist(artist):
-    graph.addNode(artist.name, radius=10)
+# def startGraph(artist):
+#     graph.view()
+#     firstArtist(artist)
 
-# add edge to the nodes
-def addConnection(artist, other_artist, album):
-    pass
+# def firstArtist(artist):
+#     graph.addNode(artist.name, radius=20)
 
-def clear():
-    graph.clear()
+# def addArtist(artist):
+#     graph.addNode(artist.name, radius=10)
+
+# # add edge to the nodes
+# def addConnection(artist, other_artist, album):
+#     pass
+
+# def clear():
+#     graph.clear()
